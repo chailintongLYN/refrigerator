a4e29c403846b9262289382f0824e8503e01ef80
贺爽 <45160925+heshuangxiaoyu@users.noreply.github.com>
2021-05-10 15:48:44 +0800

    heshuang

--- ProjectName/pages/Mypage/Myadd.js
+++ ProjectName/pages/Mypage/Myadd.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import { Text, View, StyleSheet, Image, Button,ScrollView, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/AntDesign';
 import '../../common/global'
 import { TextInput } from 'react-native-gesture-handler';
+import ImagePicker from 'react-native-image-picker';
 
-const Myadd=()=>{
+
+
+const Myadd=({navigation})=>{
+    const [img,setImg]=useState('../images/apple.jpg');
+    const options = {
+        title: '请选择',
+        cancelButtonTitle: '返回',
+        takePhotoButtonTitle: '拍照',
+        chooseFromLibraryButtonTitle: '从图库中选择',
+        storageOptions: {
+            skipBackup: true,
+            path: 'images',
+        },
+    };
+    const take = () => {
+        ImagePicker.showImagePicker(options, (response) => {
+            if (response.didCancel) {
+                return;
+            } else if (response.error) {
+                console.log('Error:', response.error);
+            } else if (response.customButton) {
+                console.log('custom:', response.customButton);
+            } else {
+                const source = { uri: response.uri };
+                setImg(source.uri)
+            }
+        });
+    }
     return(
-        <ScrollView style={{backgroundColor:'white'}}>
+        <ScrollView style={{backgroundColor:white}}>
             <View style={styles.head}>
-                <Icon name='closecircleo' size={40} style={styles.close}/>
+                <Icon name='closecircleo' size={40} style={styles.close} onPress={()=>navigation.goBack()}/>
                 <Text style={{fontSize:25,marginLeft:w-340}}>新心得</Text>
                 <TouchableOpacity style={styles.submit}><Text style={{fontSize:17,color:'white'}}>发布</Text></TouchableOpacity>
             </View>
@@ -16,25 +44,51 @@ const Myadd=()=>{
                 <Image source={require('../images/logo.jpg')} style={styles.userimg}/>
                 <Text style={styles.username}>小小刀</Text>
             </View>
-            <View style={styles.jilu}>
+            <ScrollView style={styles.jilu}>
                 <TextInput
                     placeholder='记录你的下厨心得'
                     autoFocus={true}
                     style={styles.inp}
+                    multiline={true}
+                />
+            </ScrollView>
+           
+            {/* 放置图片 */}
+            <View>
+                <Image 
+                    style={{ width: ptd(200), height: ptd(225), marginTop: ptd(10),marginLeft:(w-ptd(200))/2}}
+                    source={{uri:img}}
                 />
             </View>
+             <TouchableOpacity style={styles.addimg} onPress={()=>take()}>
+                <Icon name='picture' size={45} style={{color:'white'}}/>
+                <Text style={{fontSize:20,color:'white',fontWeight:'bold',marginLeft:22}}>添加图片</Text>
+            </TouchableOpacity>
         </ScrollView>
     )
 }
 export default Myadd
 const styles=StyleSheet.create({
+    addimg:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        width:w-50,
+        height:60,
+        marginLeft:(w-(w-50))/2,
+        backgroundColor:blue,
+        marginTop:20
+    },
     jilu:{
         width:w-50,
         marginLeft:25,
-        marginTop:10
+        marginTop:10,
+        height:300,
+        backgroundColor:'white'
     },
     inp:{
-        fontSize:20
+        fontSize:20,
+        width:w-50,
     },
     username:{
         fontSize:20,
--- ProjectName/pages/Mypage/set/SetUserimg.js
+++ ProjectName/pages/Mypage/set/SetUserimg.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Text, View, StyleSheet, Image, Button,ScrollView, TouchableOpacity, ScrollViewBase} from 'react-native'
 import Icon from 'react-native-vector-icons/AntDesign';
 import '../../../common/global'
+import ImagePicker from 'react-native-image-picker';
+
+
 const SetUserimg=({navigation})=>{
+    const [img,setImg]=useState('../../images/logo.jpg')
+    const options={
+        title:'请选择',
+        concelButtonTitle:'返回',
+        takePhotoButtonTitle:'拍照',
+        chooseFromLibraryButtonTitle:'从图库中选择',
+        storageOptions:{
+            skipBackup:true,
+            path:'images',
+        },
+    }
+    const take=()=>{
+        ImagePicker.showImagePicker(options,(response)=>{
+            if(response.didCancel){
+                return
+            }else if(response.error){
+                console.log('Error:',response.error);
+            }else if(response.customButton){
+                console.log('custom:',response.customButton);
+            }else{
+                const source={uri:response.uri};
+                setImg(source.uri)
+            }
+        })
+    }
     return(
         <ScrollView>
             <View style={styles.header}>
@@ -11,10 +39,10 @@ const SetUserimg=({navigation})=>{
             </View>
             <View style={styles.container}>
                 <View style={styles.img}>
-                    <Text>放置图片</Text>
+                    <Image source={{uri:img}} style={styles.userimg}/>
                 </View>
                 <TouchableOpacity style={styles.btn}>
-                    <Text style={{fontSize:30}}>修改头像</Text>
+                    <Text style={{fontSize:30,color:'white'}} onPress={(()=>take())}>修改头像</Text>
                 </TouchableOpacity>
             </View>
         </ScrollView>
@@ -29,7 +57,11 @@ const styles=StyleSheet.create({
         borderRadius:50,
         justifyContent:'center',
         alignItems:'center',
-        borderWidth:1
+        backgroundColor:blue
+    },
+    userimg:{
+        width:ptd(270),
+        height:300,
     },
     img:{
         width:ptd(270),
